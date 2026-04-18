0e49246a363152f5b82fef7b29eeea130a48756c
santhoshneela <santhosh.mtechcse@gmail.com>
2026-04-18 08:51:19 -0400

    github actions updated with python setup and running script

--- app.py
+++ app.py
@@ -1,1 +1,4 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
 print("git hubdemo")
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", google_api_key=os.getenv("GOOGLE_API_KEY"))
+print(llm.invoke("What is the capital of France?"))
